ea1e7a68c0b1d9d77b23098de1f1a119b34af931
Arpan050 <arpanpaul12345@gmail.com>
2025-11-22 01:01:07 +0530

    new

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -61,7 +61,7 @@ const App = () => {
             path="/category/:categoryName"
             element={
               <ProtectedRoute allowedRoles={["user"]}>
-                <Categorypage />
+                <CategoryPage />
               </ProtectedRoute>
             }
           />
